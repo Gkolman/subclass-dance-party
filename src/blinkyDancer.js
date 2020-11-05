f1cc9f09104e4381b7cad68d2d06874b1bfe6829
Gkolman <Gkolman17@gmail.com>
2020-11-05 15:09:18 -0800

    Complete pseudoclassical refactor

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,27 +1,34 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-
-  var blinkyDancer = new makeDancer(top, left, timeBetweenSteps);
-
-  makeDancer.call(this, top, left, timeBetweenstpes);
-
-  // makeDancer.this -> makeBlinkyDancer.this
-
-
-
-
+  makeDancer.call(this, top, left, timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+  // var oldStep = blinkyDancer.step;
+  //this.step();
 
-  var oldStep = blinkyDancer.step;
+};
 
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
+makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
+makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
-  return blinkyDancer;
+makeBlinkyDancer.prototype.step = function() {
+
+
+  makeDancer.prototype.step.call(this);
+  // oldStep -> reference to makeDancer.step -> was invoked
+
+  //invoked old step
+
+  //setTimeout(this.step, this.timeBetweenSteps);
+  //this.step();
+  // we need to invoke the old version of step() which, will set the position.
+  // make the constructor blink
+
+  this.$node.toggle();
+  // call the old version of step at the beginning of any call to this new version of step
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  // visible
+  // not visible
+  //
 };
--- src/dancer.js
+++ src/dancer.js
@@ -1,41 +1,37 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps) {
-  //var dancer = {};
   this.timeBetweenSteps = timeBetweenSteps;
   this.top = top;
   this.left = left;
-  // empty object
-  // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
-  // adding a jquerry selector to dancer object
+  this.step();
+  this.setPosition(top, left);
 };
 
-   makeDancer.prototype.step = function() {
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(this.step, this.timeBetweenSteps);
-  };
-  // step sets the time in between steps
-  // dancer.step();
+makeDancer.prototype.step = function() {
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  var that = this;
+  setTimeout(function() {
+    that.step();
+  }
+  , this.timeBetweenSteps);
+};
+// step sets the time in between steps
+// dancer.step();
 
-  // invokes the step method within dancer
-  makeDancer.prototype.setPosition = function(top, left) {
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    // dancer.$node === $('<span class="dancer"></span>');
-    // dancer.$node -> $('<span class="dancer"></span>');
-    // $('<span class="dancer"></span>').css(styleSettings)
-    this.$node.css(styleSettings);
+// invokes the step method within dancer
+makeDancer.prototype.setPosition = function(top, left) {
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  var styleSettings = {
+    top: top,
+    left: left
   };
-  // sets the position of the node to be what is passed in when the function is invoked
-  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
-  // this one sets the position to some random default point within the body
-  makeDancer.setPosition(this.top, this.left);
-  // function invocation ^^ -> dancer.position =   dancer.setPosition(top, left);
-  // return our superclass object - > constructor function / class
-  makeDancer.step();
+  this.$node.css(styleSettings);
+};
+
+
+// invoked setpostion WITHIN the constructor function immeditately after it was defined
+
+// invoked setpostion WITHIN the constructor function immeditately after it was defined
